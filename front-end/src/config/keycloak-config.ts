f891935862daf7184e5c77bdc9e80b60b982c0f1
Franz Ka <koehler.francois@gmail.com>
2024-08-10 14:36:53 -0700

    frontend: amélioration de code

--- front-end/src/config/keycloak-config.ts
+++ front-end/src/config/keycloak-config.ts
@@ -1,7 +1,7 @@
 const keycloakConfig = {
     url: import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:8180',
-    realm: import.meta.env.VUE_APP_KEYCLOAK_REALM || 'vue',
-    clientId: import.meta.env.VUE_APP_KEYCLOAK_CLIENT_ID || 'vuejs'
+    realm: import.meta.env.VITE_KEYCLOAK_REALM || 'vue',
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'vuejs'
 }
   
 export default keycloakConfig
